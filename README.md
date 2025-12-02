b2a1ffc5ff03158ba5dad577eaef63ad95a54208
whatisRT <andre.knispel@gmx.de>
2025-12-02 12:51:02 +0100

    Add `warnUntracked`

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Pagda is a package manager built on top of [agda.nix][https://github.com/input-o
 
 There are three ways to set options for Pagda: a global configuration file, a configuration file local to the project and command line options. The syntax for command line options is `--<name> <value>` and the syntax for configuration files is `name=value;`, each on a separate line.
 
-| Name         | Possible values  | Default | Description                                     |
-|--------------|------------------|---------|-------------------------------------------------|
-| useUntracked | true, false, ask | ask     | What to do with files that are untracked by git |
+| Name             | Possible values  | Default | Description                                             |
+|------------------|------------------|---------|---------------------------------------------------------|
+| useUntracked     | true, false, ask | ask     | What to do with files that are untracked by git         |
+| useWarnUntracked | true, false      | true    | Print a warning if certain files are not tracked by git |
--- pagda.py
+++ pagda.py
@@ -5,13 +5,23 @@ from pathlib import Path
 import re
 import subprocess
 
-default_config = {"useUntracked": "ask"}
+default_config = {"useUntracked": "ask", "warnUntracked": "true"}
 
 def has_nix():
     return subprocess.call(["which", "nix"], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT) == 0
 
+def get_untracked():
+    return subprocess.check_output(["git", "ls-files", "--others"])
+
 def has_uncommitted_files():
-    return subprocess.check_output(["git", "ls-files", "--others"]) != ""
+    return get_untracked() != ""
+
+def warn_untracked():
+    untracked = get_untracked().decode("utf-8").splitlines()
+    warn = [s for s in untracked if Path(s).name in ["flake.nix", "flake.lock", "pagda.nix"]]
+    if warn != []:
+        print("The following files which are recommended to be part of your repository are untracked:")
+        print("  ", ', '.join(warn))
 
 def copy_transform(template, target, transform):
     with open(template, "r") as f:
@@ -52,14 +62,12 @@ def get_configuration(args):
     if project_config.is_file():
         config.update(parse_config(project_config))
 
-    args_config = { "useUntracked": args.useUntracked }
+    args_config = { "useUntracked": args.useUntracked, "warnUntracked": args.warnUntracked }
     config.update((k,v) for k,v in args_config.items() if v is not None)
 
     return config
 
-def onInit(args):
-    config = get_configuration(args)
-
+def onInit(args, config):
     template_dir = Path(__file__).parents[0] / "template"
     os.makedirs(args.project_root, exist_ok=True)
     subst = lambda content: re.sub("example", args.project_name, content)
@@ -81,11 +89,10 @@ def onInit(args):
                    os.path.join(args.project_root, "Test.agda"),
                    id)
 
-def onDebug(args):
+def onDebug(args, config):
     print("Args:", args)
     project_root = get_project_root()
     print("Project root:", project_root)
-    config = get_configuration(args)
     print("Config:", config)
 
 def get_use_untracked(config):
@@ -110,19 +117,15 @@ def build_derivation(config, args):
 
     return f"{prefix}.#{args.derivation}"
 
-def run_nix(cmd, args):
-    config = get_configuration(args)
-
+def run_nix(cmd, args, config):
     subprocess.run(f"nix --experimental-features 'nix-command flakes' {cmd} {build_derivation(config, args)}", shell=True, check=True)
 
-def main():
-    if not has_nix():
-        print("Nix not found. Please install Nix before proceeding.")
-        return
+def parser():
 
     parser = argparse.ArgumentParser()
     parser.suggest_on_error = True
     parser.add_argument("--useUntracked", choices=["true", "false", "ask"], help="What to do with files that are untracked by git")
+    parser.add_argument("--warnUntracked", choices=["true", "false"], help="Print a warning if certain files are not tracked by git")
     subparsers = parser.add_subparsers(dest="command")
 
     init_parser = subparsers.add_parser("init")
@@ -138,22 +141,42 @@ def main():
     subparsers.add_parser("debug")
     subparsers.add_parser("help")
 
-    args = parser.parse_args()
+    return parser
 
-    if args.command == "init":
-        onInit(args)
+def main():
+    try:
+        p = parser()
+        args = p.parse_args()
 
-    elif args.command == "help":
-        parser.print_help()
+        if args.command == "help":
+            p.print_help()
+            return
 
-    elif args.command == "debug":
-        onDebug(args)
+        if args.command == "init":
+            onInit(args, config)
+            return
 
-    elif args.command == "build":
-        run_nix("build", args)
+        if args.command == "debug":
+            onDebug(args, config)
+            return
 
-    elif args.command == "shell":
-        run_nix("develop", args)
+        if not has_nix():
+            print("Nix not found. Please install Nix before proceeding.")
+            return
+
+        config = get_configuration(args)
+
+        if config["warnUntracked"] == "true":
+            warn_untracked()
+
+        if args.command == "build":
+            run_nix("build", args, config)
+
+        elif args.command == "shell":
+            run_nix("develop", args, config)
+
+    except KeyboardInterrupt:
+        print("Interrupted")
 
 if __name__ == "__main__":
     main()
